58854380e6bf2d857afec1e746c6ff732025afe2
Maxgamerboy1 <phillipstom60@gmail.com>
2018-06-26 23:10:49 +0100

    Rename and lock movement
    Renamed movement wand to TouchpadMovementInput and locked down the translation vector to the X/Z plane

---
+++ Assets/TouchpadMovementInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchpadMovementInput : MonoBehaviour {
+
+    public SteamVR_TrackedController Controller { get { return GetComponent<SteamVR_TrackedController>(); } }
+
+    // Use this for initialization
+    void Start () {
+	}
+    
+
+    // Update is called once per frame
+    void Update () {
+        if (Controller.padPressed)
+        {
+            var _Parent = GetComponentInParent<MeshRenderer>();
+            var camera = _Parent.GetComponentInChildren<SteamVR_Camera>();
+            var directionToMove = camera.GetRay().direction;
+            directionToMove.Scale(new Vector3(.016F, 0, .016F));
+            _Parent.transform.Translate(directionToMove);
+        }
+    }
+}
